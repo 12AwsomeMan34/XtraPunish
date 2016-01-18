d390ba305cda1d25cc79e2d6ca48415cc11db92c
12AwesomeMan34 <awsomeman149@gmail.com>
2016-01-18 16:47:49 -0500

    Implement PlayerExplode

--- src/main/java/com/awesomeman/xtrapunish/punish/PlayerExplode.java
+++ src/main/java/com/awesomeman/xtrapunish/punish/PlayerExplode.java
@@ -1,11 +1,19 @@
 package com.awesomeman.xtrapunish.punish;
 
+import java.util.Optional;
+
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntityTypes;
+import org.spongepowered.api.entity.explosive.PrimedTNT;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.cause.Cause;
+import org.spongepowered.api.event.cause.NamedCause;
+import org.spongepowered.api.world.extent.Extent;
 
 /**
  * Spawns tnt that will instantly explode on the player's location.
@@ -14,6 +22,13 @@ public class PlayerExplode implements CommandExecutor {
     
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
         Player player = args.<Player>getOne("player").get();
+        Extent extent = player.getLocation().getExtent();
+        Optional<Entity> optional = extent.createEntity(EntityTypes.PRIMED_TNT, player.getLocation().getPosition());
+        if(optional.isPresent()) {
+            PrimedTNT tnt = (PrimedTNT) optional.get();
+            // Temporary, update when spawncause is in api and impl
+            extent.spawnEntity(tnt, Cause.of(NamedCause.of("Explosion", this)));
+        }
         return CommandResult.success();
     }
 }
