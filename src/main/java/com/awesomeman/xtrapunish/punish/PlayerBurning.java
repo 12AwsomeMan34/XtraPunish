dcd57f7cea57dd77cb6ded1d96e9c54d0e268393
12AwesomeMan34 <awsomeman149@gmail.com>
2016-01-18 00:09:40 -0500

    Implement PlayerBurning

--- src/main/java/com/awesomeman/xtrapunish/punish/PlayerBurning.java
+++ src/main/java/com/awesomeman/xtrapunish/punish/PlayerBurning.java
@@ -1,10 +1,12 @@
 package com.awesomeman.xtrapunish.punish;
 
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.data.manipulator.mutable.entity.IgniteableData;
 import org.spongepowered.api.entity.living.player.Player;
 
 /**
@@ -14,6 +16,9 @@ public class PlayerBurning implements CommandExecutor {
     
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
         Player player = args.<Player>getOne("player").get();
+        IgniteableData data = Sponge.getGame().getDataManager().getManipulatorBuilder(IgniteableData.class).get().create();
+        // Player should not last this long :P
+        player.offer(data.fireTicks().set(Integer.MAX_VALUE));
         return CommandResult.success();
     }
 }
